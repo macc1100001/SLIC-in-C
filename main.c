bec495b669571b692125bc4f6f4559f3db41491e
miguel <miguelcanul97@hotmail.com>
2026-09-01 11:39:09 -0600

    Change usage message, added a parameter option for selecting number of max iterations and debug code

--- main.c
+++ main.c
@@ -9,29 +9,38 @@
 
 #include "slic.h"
 
-void usage(char* program, int k){
-	fprintf(stderr, "Usage: %s [-k number of Superpixels] imagepath\nDefault k = %d\n", program, k);
+void usage(char* program, int k, int i){
+	fprintf(stderr, "Usage: %s [-k number of Superpixels] [-i maximum number of iterations] imagepath\nDefault k = %d\nDefault i = %d\n", program, k, i);
 }
 
 int main(int argc, char** argv){
 	
 	int opt, k = 500;
 	char* image_path;
-	while((opt = getopt(argc, argv, "k:")) != -1){
+    int max_iters = 50;
+	while((opt = getopt(argc, argv, "i:k:")) != -1){
 		switch(opt){
 			case 'k':
 				k = atoi(optarg);
 				break;
+            case 'i':
+                max_iters = atoi(optarg);
+                break;
 			default:
-				usage(argv[0], k);
+				usage(argv[0], k, max_iters);
 				exit(EXIT_FAILURE);
 		}
 	}
 	if(optind >= argc){
 		fprintf(stderr, "Expected arguments after options\n");
-		usage(argv[0], k);
+		usage(argv[0], k, max_iters);
 		exit(EXIT_FAILURE);
 	}
+    if(k == 0 || max_iters == 0){
+       fprintf(stderr, "Error reading arguments\n");
+       usage(argv[0], k, max_iters);
+       exit(EXIT_FAILURE);
+    }
 	image_path = argv[optind];
 	
 	int x_dim, y_dim, n_channels;
@@ -48,7 +57,7 @@ int main(int argc, char** argv){
 	int sizeC;
 	struct cluster* centers;
 
-	float* segmented = SLIC(cielab, x_dim, y_dim, n_channels, k, 10.0, centers, &sizeC);
+	float* segmented = SLIC(cielab, x_dim, y_dim, n_channels, k, 10.0, &centers, &sizeC, max_iters);
 	if(segmented == NULL){
 		fprintf(stderr, "Error allocating memory\n");
 		exit(EXIT_FAILURE);
@@ -56,13 +65,16 @@ int main(int argc, char** argv){
 
 	unsigned char* segmented_rgb = cielab_to_rgb(segmented, x_dim, y_dim, n_channels);
 
+#ifndef DEBUG
 	int result_seg = stbi_write_jpg("segmented.jpeg", x_dim, y_dim, n_channels, segmented_rgb, 100);
+#endif
+    printf("Done...\n");
 
 #ifdef DEBUG	
 	for(int i = 0; i < sizeC; ++i){
-		paintPixel(image_data, x_dim, y_dim, n_channels, (centers+i)->x, (centers+i)->y, 255, 0, 0);
+		paintPixel(segmented_rgb, x_dim, y_dim, n_channels, (centers+i)->x, (centers+i)->y, 255, 0, 0);
 	}
-	int result = stbi_write_jpg("segmented_mod.jpeg", x_dim, y_dim, n_channels, image_data, 100);
+	int result = stbi_write_jpg("segmented_mod.jpeg", x_dim, y_dim, n_channels, segmented_rgb, 100);
 #endif
 	
 	
